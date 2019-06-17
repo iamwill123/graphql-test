334ca1d82de20ae512bef2d0ef14e7d4e1d0642b
William Pei Yuan <williampeiyuan@gmail.com>
2019-06-17 14:48:33 -0400

    UPDATES - Example mutation

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ console.log({ starting: true });
 import express from 'express';
 const app = express();
 import graphqlHTTP from 'express-graphql';
-import { GraphQLObjectType, GraphQLString, GraphQLSchema } from 'graphql';
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLID
+} from 'graphql';
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
@@ -14,12 +20,47 @@ const RootQuery = new GraphQLObjectType({
       resolve() {
         return 'viewer!';
       }
+    },
+    node: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve(source, args) {
+        return inMemoryStore[args.key];
+      }
+    }
+  }
+});
+
+let inMemoryStore = {};
+const RootMutation = new GraphQLObjectType({
+  name: 'RootMutation',
+  description: 'The root mutation',
+  fields: {
+    setNode: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        },
+        value: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(source, args) {
+        inMemoryStore[args.key] = args.value;
+        return inMemoryStore[args.key];
+      }
     }
   }
 });
 
 const Schema = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: RootMutation
 });
 
 app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }));
