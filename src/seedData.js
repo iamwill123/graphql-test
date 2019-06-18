757aabe014f899a613050881fed86f797940c159
William Pei Yuan <williampeiyuan@gmail.com>
2019-06-18 18:38:23 -0400

    UPDATES - Database setup

---
+++ src/seedData.js
@@ -0,0 +1,45 @@
+import * as data from './data';
+import * as tables from './tables';
+import * as database from './database';
+
+const sequencePromises = function(promises) {
+  return promises.reduce((promise, promiseFunction) => {
+    return promise.then(() => {
+      return promiseFunction();
+    });
+  }, Promise.resolve());
+};
+
+// the exports of tables.js are the objects created by node-sql.
+const createDatabase = () => {
+  // to create a database query with node-sql, we use the .toQuery() function and then pass it into the getSql function
+  let promises = [tables.users, tables.usersFriends, tables.posts].map(
+    table => {
+      return () => database.getSql(table.create().toQuery());
+    }
+  );
+  return sequencePromises(promises);
+};
+
+const insertData = () => {
+  let { users, posts, usersFriends } = data;
+
+  let queries = [
+    tables.users.insert(users).toQuery(),
+    tables.posts.insert(posts).toQuery(),
+    tables.usersFriends.insert(usersFriends).toQuery()
+  ];
+
+  let promises = queries.map(query => {
+    return () => database.getSql(query);
+  });
+  return sequencePromises(promises);
+};
+
+createDatabase()
+  .then(() => {
+    return insertData();
+  })
+  .then(() => {
+    console.log({ done: true });
+  });
